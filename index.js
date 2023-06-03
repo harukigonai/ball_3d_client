3a1b722197f55d95e37c170d2136664d61054121
harukigonai <hg2541@columbia.edu>
2023-06-02 23:27:46 -0400

    Write Backend for Team Selection Pages

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ ball = new Ball({
 })
 ballMap.set(ball.uuid, ball)
 
+const playerMap = new Map()
+
 const httpServer = createServer()
 
 const io = new Server(httpServer, {
@@ -29,53 +31,156 @@ const io = new Server(httpServer, {
 
 io.on('connection', (socket) => {
     const player = new Player()
-    socketToPlayer[socket] = player
+    playerMap.set(player.uuid, player)
 
     socket.join('game')
+    console.log(`Player ${player.uuid} connected.`)
 
-    socket.send(
-        JSON.stringify({
-            type: 'initBallMap',
-            content: constructClientBallMap(),
-        })
+    socket.on('disconnect', () => {
+        console.log(`Player ${player.uuid} disconnected.`)
+        playerMap.delete(player.uuid)
+    })
+
+    socket.on('enter-name', (data) => {
+        const packet = JSON.parse(data)
+        const username = packet.username
+
+        if (username == '')
+            console.log(`Player ${player.uuid} set invalid username.`)
+        else {
+            console.log(`Player ${player.uuid} set username to ${username}`)
+            player.username = username
+
+            broadcastTeamSelectionInfo(socket)
+        }
+    })
+
+    socket.on('select-team', (data) => {
+        const packet = JSON.parse(data)
+        const team = packet.team
+
+        if (player.username == '') {
+            console.log(
+                `Player ${player.uuid} selected team before setting username.`
+            )
+        } else if (team != 'red' && team != 'blue')
+            console.log(`Player ${player.uuid} selected invalid team.`)
+        else {
+            console.log(`Player ${player.uuid} set team to ${team}.`)
+            player.team = team
+            player.ready = false
+
+            broadcastTeamSelectionInfo(socket)
+        }
+    })
+
+    socket.on('request-team-selection-info', (data) =>
+        broadcastTeamSelectionInfo(socket)
     )
 
+    socket.on('confirm-ready', (data) => {
+        const packet = JSON.parse(data)
+        const ready = packet.ready
+
+        console.log(ready)
+
+        if (player.username == '' || player.team == '') {
+            console.log(
+                `Player ${player.uuid} confirmed ready before entering username or selecting team.`
+            )
+        } else if (ready != false && ready != true)
+            console.log(`Player ${player.uuid} selected invalid ready state.`)
+        else {
+            console.log(`Player ${player.uuid} set ready state to ${ready}`)
+            player.ready = ready
+
+            broadcastTeamSelectionInfo(socket)
+
+            if (player.ready) {
+                // Are all players ready?
+                let allReady = true
+
+                // Potential race condition if someone sends not ready around here?
+                playerMap.forEach(
+                    (player, _) => (allReady = allReady && player.ready)
+                )
+
+                if (allReady) {
+                    // Start the game
+                    console.log('Sending start-game')
+                    io.sockets.in('game').emit('start-game', JSON.stringify({}))
+                }
+            }
+        }
+    })
+
+    socket.on('ready-to-start-game', (data) => {
+        socket.emit(
+            'init',
+            JSON.stringify({
+                ballMap: constructClientBallMap(),
+                playerMap: constructClientPlayerMap(player.uuid),
+            })
+        )
+    })
+
     // receive a message from the client
-    socket.on('message', (data) => {
+    socket.on('updatePlayer', (data) => {
+        const packet = JSON.parse(data)
+        console.log('updatePlayer', packet)
+
+        player.position = packet.position
+
+        socket.to('game').emit(
+            'updatePlayer',
+            JSON.stringify({
+                uuid: player.uuid,
+                position: player.position,
+            })
+        )
+    })
+
+    // receive a message from the client
+    socket.on('updateBall', (data) => {
         const packet = JSON.parse(data)
         // console.log(packet)
 
-        switch (packet.type) {
-            case 'updatePlayer':
-                player.position = packet.content.position
-                // console.log(player.position)
+        const uuid = packet.uuid
+        const ball = ballMap.get(uuid)
+        ball.position = packet.position
+        ball.vel = packet.vel
 
-                socket.to('game').emit(
-                    'message',
-                    JSON.stringify({
-                        playerID: player.uuid,
-                        position: player.position,
-                    })
-                )
-                break
-            case 'updateBall':
-                const uuid = packet.content.uuid
-                const ball = ballMap.get(uuid)
-                ball.position = packet.content.position
-
-                socket.to('game').emit(
-                    'message',
-                    JSON.stringify({
-                        playerID: ball.uuid,
-                        position: ball.position,
-                    })
-                )
-                break
-        }
+        socket.to('game').emit('updateBall', data)
     })
 })
 
-httpServer.listen(3000)
+httpServer.listen(4000)
+
+const broadcastTeamSelectionInfo = (socket) => {
+    const redTeam = []
+    const blueTeam = []
+    const unselectedTeam = []
+
+    playerMap.forEach((player, _) => {
+        const playerInfo = {
+            username: player.username,
+            ready: player.ready,
+        }
+        if (player.team == 'red') redTeam.push(playerInfo)
+        else if (player.team == 'blue') blueTeam.push(playerInfo)
+        else unselectedTeam.push(playerInfo)
+    })
+
+    console.log('Emitting team-selection-info')
+    io.sockets.in('game').emit(
+        'team-selection-info',
+        JSON.stringify({
+            redTeam: redTeam,
+            blueTeam: blueTeam,
+            unselectedTeam: unselectedTeam,
+        })
+    )
+}
 
 const constructClientBallMap = () => {
     const clientBallMap = {}
@@ -89,4 +194,19 @@ const constructClientBallMap = () => {
     })
 
     return clientBallMap
+}
+
+const constructClientPlayerMap = (playerUuid) => {
+    const clientPlayerMap = {}
+
+    playerMap.forEach((player, uuid) => {
+        clientPlayerMap[uuid] = {
+            playable: uuid == playerUuid,
+            uuid: player.uuid,
+            position: player.position,
+            vel: player.vel,
+        }
+    })
+
+    return clientPlayerMap
 }
--- player.js
+++ player.js
@@ -5,9 +5,15 @@ let playerID = 0
 export class Player {
     position
     uuid
+    username
+    team
+    ready
 
     constructor() {
         this.position = new THREE.Vector3()
         this.uuid = playerID++
+        this.username = ''
+        this.team = ''
+        this.ready = false
     }
 }
